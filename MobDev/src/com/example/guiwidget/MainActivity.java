6769a3b34a2f1e096e99cb9ab5a9c1f0763c5a0a
Mary <mcruthven@gmail.com>
2012-09-26 16:03:24 -0400

    update

--- MobDev/src/com/example/guiwidget/MainActivity.java
+++ MobDev/src/com/example/guiwidget/MainActivity.java
@@ -18,16 +18,25 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "Transition";
-	private int pinkLevel,blueLevel,yellowLevel;
+	private int pinkLevel=255,blueLevel=255,yellowLevel=255;
+	private boolean setvar=true;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// checkbox that disables all other buttons and check boxes
 		addListenerOnDisableCheck();
+		
+		// button that changes clock between digital and analog
 		addListenerOnClocks();
+		
+		// change background based on check boxes and the seekbar
 		seekBarChange();
 	}
 
+	// when checked disable everything
 	public void addListenerOnDisableCheck() {
 
 		CheckBox checkbox= (CheckBox) findViewById(R.id.disableCheck);
@@ -35,28 +44,35 @@ public class MainActivity extends Activity {
 		checkbox.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				
+				// when checkbox is checked, disables
+				// when it is not checked it enables
 				boolean enableStatus=!((CheckBox) v).isChecked();
+				
+				// changing statuses of buttons between enabled and disabled
 				findViewById(R.id.pinkCheck).setEnabled(enableStatus);
 				findViewById(R.id.yellowCheck).setEnabled(enableStatus);
 				findViewById(R.id.blueCheck).setEnabled(enableStatus);
-				findViewById(R.id.background_button).setEnabled(enableStatus);
+				findViewById(R.id.background_button).setEnabled
+				(enableStatus);
 				findViewById(R.id.seekBar1).setEnabled(enableStatus);
 			}
 		});
-
 	}
+	
+	// modifies background color from seek bar values
 	public void seekBarChange(){
 		SeekBar seekbar=(SeekBar) findViewById(R.id.seekBar1);
 		seekbar.setOnSeekBarChangeListener( new OnSeekBarChangeListener()
 		{
+			// progress bar value
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser)
 			{
-				Log.d(TAG,"changed"+progress);
-				int bar_related=((int)(255*(1-progress/100.0)));
-				blueLevel=255;
-				yellowLevel=255;
-				pinkLevel=255;
+				// bar value converted into color level
+				int bar_related=((int)(255*(1-progress/100.0)));	
+				
+				// checked boxes make that color dependent on the bar value
 				if(((CheckBox)findViewById(R.id.pinkCheck)).isChecked())
 					pinkLevel=bar_related;
 				if(((CheckBox)findViewById(R.id.yellowCheck)).isChecked())
@@ -64,7 +80,9 @@ public class MainActivity extends Activity {
 				if(((CheckBox)findViewById(R.id.blueCheck)).isChecked())
 					blueLevel=bar_related;
 				
-				findViewById(R.id.background).setBackgroundColor(Color.argb( 255,blueLevel ,pinkLevel,yellowLevel));
+				// set background color based on bar position
+				findViewById(R.id.background).setBackgroundColor(Color.argb( 255,
+						blueLevel ,pinkLevel,yellowLevel));
 			}
 			public void onStartTrackingTouch(SeekBar seekBar)
 			{
@@ -75,6 +93,8 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
+	
+	// changes clock
 	public void addListenerOnClocks() {
 
 		Button checkbox= (Button) findViewById(R.id.background_button);
@@ -85,14 +105,10 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				if(findViewById(R.id.analogClock1).getVisibility()==View.INVISIBLE){
 					((Button) findViewById(R.id.background_button)).setText(string.digital);
-					//		CheckBox temp=(CheckBox)findViewById(R.id.checkToRadio);
-					//		temp.setChecked(false);
 					findViewById(R.id.digitalClock).setVisibility(View.INVISIBLE);
 					findViewById(R.id.analogClock1).setVisibility(View.VISIBLE);
 				}else{
 					((Button) findViewById(R.id.background_button)).setText(string.analog);
-					//	CheckBox temp=(CheckBox)findViewById(R.id.checkToRadio);
-					//	temp.setChecked(false);
 					findViewById(R.id.analogClock1).setVisibility(View.INVISIBLE);
 					findViewById(R.id.digitalClock).setVisibility(View.VISIBLE);
 				}
